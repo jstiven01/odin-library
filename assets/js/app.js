11acc07f065ae2b164b41a81a7ca8ea5bf60b40c
jstiven01 <stiven10@gmail.com>
2019-12-17 16:10:27 -0500

    Creating new form to add new book

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,20 +14,19 @@ function addBookToLibrary(book) {
 
 function render() {
   let table = document.querySelector(".book-list");
-  generateTable(table, myLibrary);
+  generateTable(table, myLibrary[myLibrary.length-1]);
 }
 
-function generateTable(table, data) {
-  for (let element of data) {
+function generateTable(table, element) {
+
     let row = table.insertRow();
     for (key in element) {
       let cell = row.insertCell();
       let text = document.createTextNode(element[key]);
       cell.appendChild(text);
     }
-  }
 }
-
+/* 
 book1 = new Book("Harry Potter I", "Rowling", "880", "Read");
 book2 = new Book("Harry Potter II", "Rowling", "780", "Read");
 book3 = new Book("Harry Potter III", "Rowling", "80", "Read");
@@ -36,5 +35,18 @@ addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 render();
+*/
 
 console.log(myLibrary)
+
+const button_newBook = document.querySelector("#new-book");
+function addBookEvent(){
+    console.log(this);
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const newBook = new Book(title.value, author.value, pages.value, "Unread");
+    addBookToLibrary(newBook);
+    render();
+}
+button_newBook.addEventListener('click', addBookEvent);
