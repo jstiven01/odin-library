1bb293af1744262347be7607ca238326b7b859b8
vieiramanda11 <amanda_vieira@outlook.com>
2019-12-17 17:39:41 -0300

    create render function and table to display the books

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,22 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function render() {
+  let table = document.querySelector(".book-list");
+  generateTable(table, myLibrary);
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
 book1 = new Book("Harry Potter I", "Rowling", "880", "Read");
 book2 = new Book("Harry Potter II", "Rowling", "780", "Read");
 book3 = new Book("Harry Potter III", "Rowling", "80", "Read");
@@ -19,5 +35,6 @@ book3 = new Book("Harry Potter III", "Rowling", "80", "Read");
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
+render();
 
 console.log(myLibrary)
