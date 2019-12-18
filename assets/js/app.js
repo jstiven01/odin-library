65041ec84b47d3b02a7b53d0332fd9171d527245
jstiven01 <stiven10@gmail.com>
2019-12-18 11:30:05 -0500

    Running Prettier on the code

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,11 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+  myLibrary.push(book);
 }
 
-function deleteBookFromLibrary(index){
-    myLibrary.splice(index, 1);
+function deleteBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
 }
 
 function updateStatusInLibrary(index, status) {
@@ -20,88 +20,76 @@ function updateStatusInLibrary(index, status) {
 }
 
 function render(newBook = null) {
-
   let table = document.querySelector(".body-book-list");
   if (newBook == null) {
-    for(let i=0; i< myLibrary.length; i++){
+    for (let i = 0; i < myLibrary.length; i++) {
       generateTable(table, myLibrary[i], i);
     }
-  }else{
-    generateTable(table, newBook, myLibrary.length-1);
+  } else {
+    generateTable(table, newBook, myLibrary.length - 1);
   }
 }
 
 function generateTable(table, element, index) {
-
-    let row = table.insertRow();
-    let button = document.createElement('button');
-    let statusButton = document.createElement('button')
-    statusButton.className = 'status-button'
-    statusButton.textContent = 'Unread';
-    statusButton.setAttribute('data-index', index)
-    button.className = "delete-button"
-    button.setAttribute('data-index', index)
-    button.innerHTML = 'Delete';
-    for (let key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      if (key == 'status') {
-        cell.appendChild(statusButton);
-      } else {
-        cell.appendChild(text);
-      }
+  let row = table.insertRow();
+  let button = document.createElement("button");
+  let statusButton = document.createElement("button");
+  statusButton.className = "status-button";
+  statusButton.textContent = "Unread";
+  statusButton.setAttribute("data-index", index);
+  button.className = "delete-button";
+  button.setAttribute("data-index", index);
+  button.innerHTML = "Delete";
+  for (let key in element) {
+    let cell = row.insertCell();
+    let text = document.createTextNode(element[key]);
+    if (key == "status") {
+      cell.appendChild(statusButton);
+    } else {
+      cell.appendChild(text);
     }
-    let buttonCell = row.insertCell();
-    buttonCell.appendChild(button);
+  }
+  let buttonCell = row.insertCell();
+  buttonCell.appendChild(button);
 }
 
-
 const button_newBook = document.querySelector("#new-book");
-const table_bookList = document.querySelector(".body-book-list")
+const table_bookList = document.querySelector(".body-book-list");
 
-function addBookEvent(){
-
+function addBookEvent() {
   const title = document.querySelector("#title");
   const author = document.querySelector("#author");
   const pages = document.querySelector("#pages");
-  if(title.value != "" && author.value !="" && pages.value !=""){
+  if (title.value != "" && author.value != "" && pages.value != "") {
     const newBook = new Book(title.value, author.value, pages.value, "Unread");
-    title.value  = ""; 
-    author.value  = "";
-    pages.value  = "";
+    title.value = "";
+    author.value = "";
+    pages.value = "";
     addBookToLibrary(newBook);
     render(newBook);
   }
 }
 
-function tableEvent(event){
-
-    if (event.target.classList.contains('delete-button')) {
-
-    let index_to_delete = event.target.dataset['index'];
+function tableEvent(event) {
+  if (event.target.classList.contains("delete-button")) {
+    let index_to_delete = event.target.dataset["index"];
 
     deleteBookFromLibrary(index_to_delete);
     this.innerHTML = "";
 
     render();
-  } else if (event.target.classList.contains('status-button')) {
+  } else if (event.target.classList.contains("status-button")) {
+    let index_to_update = event.target.dataset["index"];
 
-    let index_to_update = event.target.dataset['index'];
-
-      if (event.target.textContent == 'Unread') {
-        event.target.textContent = 'Read';
-        updateStatusInLibrary(index_to_update, 'Read');
-      } else {
-        event.target.textContent = 'Unread';
-        updateStatusInLibrary(index_to_update, 'Unread');
-      }
+    if (event.target.textContent == "Unread") {
+      event.target.textContent = "Read";
+      updateStatusInLibrary(index_to_update, "Read");
+    } else {
+      event.target.textContent = "Unread";
+      updateStatusInLibrary(index_to_update, "Unread");
     }
+  }
 }
 
-button_newBook.addEventListener('click', addBookEvent);
-table_bookList.addEventListener('click', tableEvent);
-
-
-
-
-
+button_newBook.addEventListener("click", addBookEvent);
+table_bookList.addEventListener("click", tableEvent);
